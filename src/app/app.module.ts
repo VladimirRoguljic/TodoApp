a98062f134cfb331fd2ea0493aa36c3efdf41d69
Vladimir Roguljic <vladimir.roguljic@gauss.hr>
2019-09-03 13:45:57 +0200

    adding ng2 dragula for drag&drop list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppComponent } from './app.component';
 import {TodoDataService} from './services/todo-data.service';
 import {FormsModule} from "@angular/forms";
 import {CommonModule} from "@angular/common";
+import {DragulaModule} from "ng2-dragula";
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import {CommonModule} from "@angular/common";
   imports: [
     BrowserModule,
     FormsModule,
-    CommonModule
+    CommonModule,
+    DragulaModule.forRoot()
   ],
   providers: [TodoDataService],
   bootstrap: [AppComponent]
